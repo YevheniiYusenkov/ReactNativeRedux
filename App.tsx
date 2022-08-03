9cb165d235ef64b45634235f4b212d5f33342874
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-03 03:34:07 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,42 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, Text } from 'react-native';
+import { Provider } from 'react-redux';
 
-export const App: React.FC = (): JSX.Element => {
-  return <Text></Text>;
+import {
+  SetupStore,
+  useAppDispatch,
+  useAppSelector,
+  LoginAction,
+} from '@app/state';
+
+const App: React.FC = (): JSX.Element => {
+  const { token } = useAppSelector(state => state.AuthSliceReducer);
+  const dispatch = useAppDispatch();
+
+  return (
+    <>
+      {token == '' ? (
+        <>
+          <Text>Loading...</Text>
+          <Button
+            title={'Load'}
+            onPress={() =>
+              dispatch(
+                LoginAction({ username: 'kminchelle', password: '0lelplR' }),
+              )
+            }></Button>
+        </>
+      ) : (
+        <Text>Hello World!</Text>
+      )}
+    </>
+  );
+};
+
+export const AppWrapper: React.FC = (): JSX.Element => {
+  return (
+    <Provider store={SetupStore()}>
+      <App />
+    </Provider>
+  );
 };
